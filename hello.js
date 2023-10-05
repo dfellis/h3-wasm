4a7a44c45ef7a676f4bce703f0c5d4f4c5d0cc13
David Ellis <isv.damocles@gmail.com>
2023-10-05 09:00:56 -0500

    Implement cellToBoundary (#6)

--- hello.js
+++ hello.js
@@ -4,3 +4,4 @@ console.log(test.helloWorld());
 console.log(test.greet("Zig"));
 console.log(test.latLngToCell(45, 40, 2));
 console.log(test.cellToLatLng("822d57fffffffff"));
+console.log(test.cellToBoundary("822d57fffffffff"));
