f8c1fb955e471e97a2e6a6be603d1114336e5100
David Ellis <isv.damocles@gmail.com>
2023-10-05 09:29:39 -0500

    Implement the inspection functions

--- hello.js
+++ hello.js
@@ -5,3 +5,8 @@ console.log(test.greet("Zig"));
 console.log(test.latLngToCell(45, 40, 2));
 console.log(test.cellToLatLng("822d57fffffffff"));
 console.log(test.cellToBoundary("822d57fffffffff"));
+console.log(test.getResolution("822d57fffffffff"));
+console.log(test.isValidCell("822d57fffffffff"));
+console.log(test.isResClassIII("822d57fffffffff"));
+console.log(test.isPentagon("822d57fffffffff"));
+console.log(test.getIcosahedronFaces("822d57fffffffff"));
